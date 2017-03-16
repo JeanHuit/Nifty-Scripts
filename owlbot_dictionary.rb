2b78c6b8d15b036976f4f6e90e480a0f6927dc06
Jean Huit <kollecter@gmail.com>
2017-03-16 00:57:02 +0000

    added comments

--- owlbot_dictionary.rb
+++ owlbot_dictionary.rb
@@ -1,13 +1,19 @@
 # Wrapper for the owlbot dictionary API
+
 require 'open-uri'
 require 'json'
 
-lookup = ARGV.first
+# Allows you to pass word to lookup as an argument
 
+lookup = ARGV.first
+# Function to read url and parse output
 def read_url(url)
   raw_output = open(url).read
   content = JSON.parse(raw_output)
   puts content
 end
+
+# Base url for queries to the owlbot,accepts arguement and sets format to json.
+# format could also be set to "api"
 
 read_url("https://owlbot.info/api/v1/dictionary/#{lookup}?format=json")
