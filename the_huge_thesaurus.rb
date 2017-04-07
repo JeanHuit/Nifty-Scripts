d31993140c5734310a62ef103d3f622016130f26
Jean Huit <kollecter@gmail.com>
2017-04-07 05:26:31 +0000

    added nouns, verbs and antonyms

--- the_huge_thesaurus.rb
+++ the_huge_thesaurus.rb
@@ -11,8 +11,19 @@ lookup = ARGV.first
 def read_url(url)
   raw_output = open(url).read
   content = JSON.parse(raw_output)
+  # Puts out the whole result
   puts content
+  puts '#' * 10
+  # puts out results specific to nouns
+  puts content['noun']
+  puts '#' * 10
+  # puts our verbs
+  puts content['verb']
+  puts '#' * 10
+  #  drills down into nouns and picks out the antonymn of the word
+  puts content['noun']['ant']
 end
+# Using Dotenv to load and keep api keys out of the puclic view
 my_secret_key = ENV['my_secret_key']
 
 read_url("http://words.bighugelabs.com/api/2/#{my_secret_key}/#{lookup.downcase}/json")
