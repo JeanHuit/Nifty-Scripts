2411bd08f8bc4eab5b7d1c5e65aa279dd8d8fd47
Jean Huit <kollecter@gmail.com>
2017-04-15 02:05:41 +0000

    imported code from owlbot_dictionary.rb

--- dict_test.rb
+++ dict_test.rb
@@ -10,3 +10,20 @@ def write_word
   arr_words.each { |word| dict(word.strip) }
   # dict(arr_words[593].strip)
 end
+
+
+def dict(lookup)
+  url = "https://owlbot.info/api/v1/dictionary/#{lookup.downcase}?format=json"
+  raw_output = open(url).read
+  content = JSON.parse(raw_output)
+  puts content[0]['defenition']
+rescue NoMethodError
+  puts 'bad word'
+rescue OpenURI::HTTPError
+  puts 'bad word'
+else
+  newfile = 'defined.txt'
+  File.open(newfile, 'a') { |to_append| to_append.write("#{lookup}\n") }
+end
+
+write_word
