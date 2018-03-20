d85210d8b6c969e05f5b3567261ce7979954aaa6
Jean Huit <kollecter@gmail.com>
2018-03-20 09:25:14 +0000

    strips our unnecessaries.

--- web_hash.rb
+++ web_hash.rb
@@ -1,24 +1,22 @@
-#Author: Jean Huit
-#Date: 08-02-2018
-#Dependencies: Spidr | open-uri | digest |Awesome_Print
+# Author: Jean Huit
+# Date: 08-02-2018
+# Dependencies: Spidr | open-uri | digest
 
 require 'Spidr'
 require 'open-uri'
 require 'digest'
-require "awesome_print"
 
 url_list = Hash.new { |hash, key| hash[key] = [] }
 
-
-Spidr.site('http://localhost/web/') do |spider|
+Spidr.site('http://localhost/ladma') do |spider|
   spider.every_url do |url|
     url_digest = open(url).read
     url_list[url] = Digest::SHA2.hexdigest "#{url_digest}"
   end
 end
 
-puts url_list.size
-ap url_list
+puts url_list
+
 
 # TODO: Connect to some of database for persistence
 # TODO: Make up a simple web interface for this
