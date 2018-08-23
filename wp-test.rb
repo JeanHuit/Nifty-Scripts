7aabeba2b3c61457810b10ece9dfc7c588a87a7e
Jean Huit <kollecter@gmail.com>
2018-08-23 13:43:59 +0000

    ruby implementation of wordpress's default password generator.

---
+++ wp-test.rb
@@ -0,0 +1,16 @@
+
+def wp_generate_password(len = 12)
+  chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
+  chars = chars.split('')
+  a = 0
+  password = ''
+  while a < len
+    xfactor = Random.new
+    index = xfactor.rand(chars.length)
+    password << chars[index]
+    a += 1
+  end
+  File.open('wp-pass.txt', 'a') { |to_append| to_append.write("#{password},\n") }
+end
+
+Array.new(100000) { wp_generate_password }
