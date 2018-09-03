28539f8cb3d15330932c61a4e3593c668d531c57
Jean Huit <kollecter@gmail.com>
2018-09-03 00:59:42 +0000

    minor modifications to owlbot

--- owlbot_dictionary.rb
+++ owlbot_dictionary.rb
@@ -18,3 +18,9 @@ end
 # format could also be set to "api"
 
 read_url("https://owlbot.info/api/v1/dictionary/#{lookup.downcase}?format=json")
+
+# adding wikipedia search to this
+# read_url("https://en.wikipedia.org/w/api.php?action=query
+#             &titles=#{lookup.downcase}
+#             &prop=revisions&rvprop=content&format=jsonfm&formatversion=2
+#             ")
